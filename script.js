f3fa27db0844c46c060bff26526126ea9a2fad6a
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-08-24 22:50:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function firstChar(text) {
-  // your code here
+  // Iterate through each character in the string
+  for (let i = 0; i < text.length; i++) {
+    // If the character is not a space, return it
+    if (text[i] !== ' ') {
+      return text[i];
+    }
+  }
+  // If no non-space character is found, return an empty string
+  return '';
 }
 
-// Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
-alert(firstChar(text));
+// Test cases
+console.log(firstChar(' Rosa Parks ')); // 'R'
+console.log(firstChar(' Hello World ')); // 'H'
+console.log(firstChar(' ')); // ''
+console.log(firstChar('')); // ''
+
+// Uncomment the following line to show the prompt popup
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
